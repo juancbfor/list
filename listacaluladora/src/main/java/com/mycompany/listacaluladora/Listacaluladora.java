4dab8bddf2149a56a02ee0ceb45b010efc77b078
juancbfor <110957671+juancbfor@users.noreply.github.com>
2022-08-11 02:03:15 -0500

    Update Listacaluladora.java

--- listacaluladora/src/main/java/com/mycompany/listacaluladora/Listacaluladora.java
+++ listacaluladora/src/main/java/com/mycompany/listacaluladora/Listacaluladora.java
@@ -3,23 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.listacaluladora;
+package com.mycompany.listcalc;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-
 /**
  *
  * @author juancbfor
  */
-public class Listacaluladora {
+public class Listcalc {
 
     public static void main(String[] args) {
+       
        List<Double> calculadora;
        calculadora = new ArrayList<>();
        String entrada1;
        String entrada2;
+       char decision='s';
+       
+       while(decision == 's' ){
        
        entrada1=JOptionPane.showInputDialog(null, "Introduce el primero digito");
        Double conventrada1=Double.parseDouble(entrada1);
@@ -31,11 +35,25 @@ public class Listacaluladora {
        
        String opcion=JOptionPane.showInputDialog(null, "Selecciona una de las siguientes opciones: 1. Sumar. 2. Restar. 3. Multiplicar. 4. Dividir");
        Double convopcion=Double.parseDouble(opcion);
+           
+           if(convopcion == 1.0){
+        Double operacion = calculadora.get(0)+calculadora.get(1);
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + operacion);
+       }else if(convopcion == 2.0){
+        Double operacion = calculadora.get(0)-calculadora.get(1);
+        JOptionPane.showMessageDialog(null, "El resultado de la resta es: " + operacion); 
+       } else if (convopcion==3.0){
+           Double operacion = calculadora.get(0)*calculadora.get(1);
+           JOptionPane.showMessageDialog(null, "El resultado de la multiplicación es: " + operacion); 
+       }else if(convopcion==4.0){
+           Double operacion = calculadora.get(0)/calculadora.get(1);
+           JOptionPane.showMessageDialog(null, "El resultado de la división es: " + operacion);
+       }
        
-       if(convopcion == 1){
+       decision= JOptionPane.showInputDialog(null,"Escriba s o n para seguir realizando operaciones").charAt(0);
        
        }
        
-       
-               }
+    }
+
 }
